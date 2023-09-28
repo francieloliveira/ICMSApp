61a59ac864f47672c1fa1f8701e978fb003cbb5d
Franciel <franciel.technix@gmail.com>
2023-09-28 15:12:40 -0300

    fix: * Troca de Componente para a listagem de Estados * Adicionado o resource de strings.xml * Adicionado o resource de estados.xml

--- app/src/main/java/empresalogistica/com/MainActivity.java
+++ app/src/main/java/empresalogistica/com/MainActivity.java
@@ -2,76 +2,45 @@ package empresalogistica.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mEditTextValor;
     private TextView mTextViewPorcentagem;
     private TextView mTextViewTotal;
-
-    private AutoCompleteTextView autoCompleteTextView;
+    private Spinner mSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        autoCompleteTextView = findViewById(R.id.autocompleteTextView);
-        String[] items = {"RS", "SC", "PR", "AC", "MS", "RO", "AL", "AM", "AP", "BA",
-                "CE", "DF", "ES", "GO", "MA", "MT", "MG", "PA", "PB", "PE", "PI",
-                "RJ", "RN", "RR", "SP", "SE", "TO"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, items);
-        autoCompleteTextView.setAdapter(adapter);
+        mSpinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.estados, android.R.layout.simple_spinner_item);
+        mSpinner.setAdapter(adapter);
 
         mEditTextValor = findViewById(R.id.editTextValor);
         mTextViewPorcentagem = findViewById(R.id.textViewPorcentagem);
         mTextViewTotal = findViewById(R.id.textViewTotal);
     }
 
-    @SuppressLint("DefaultLocale")
     public void calcular(View view) {
-        float porcentagem = 0;
-        String estado = autoCompleteTextView.getText().toString();
-
+        float porcentagem;
+        String estado = mSpinner.getItemAtPosition(mSpinner.getSelectedItemPosition()).toString();
         switch (estado) {
             case "AC":
             case "MS":
             case "RO":
                 porcentagem = 0.17f;
                 break;
-            case "AL":
-            case "PR":
-            case "AM":
-            case "AP":
-            case "BA":
-            case "CE":
-            case "DF":
-            case "ES":
-            case "GO":
-            case "MA":
-            case "MT":
-            case "MG":
-            case "PA":
-            case "PB":
-            case "PE":
-            case "PI":
-            case "RJ":
-            case "RN":
-            case "RS":
-            case "RR":
-            case "SC":
-            case "SP":
-            case "SE":
-            case "TO":
-                porcentagem = 0.18f;
-                break;
             default:
+                porcentagem = 0.18f;
                 break;
         }
 
