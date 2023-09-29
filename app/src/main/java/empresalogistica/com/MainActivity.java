62e95197994d74bc80ee9bca3c313cef688b663a
Franciel <franciel.technix@gmail.com>
2023-09-29 01:04:39 -0300

    fix: RN * Tratamento do erro de porcentagem não escolhida.

--- app/src/main/java/empresalogistica/com/MainActivity.java
+++ app/src/main/java/empresalogistica/com/MainActivity.java
@@ -1,6 +1,7 @@
 package empresalogistica.com;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialog;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,9 +9,9 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     private EditText mEditTextValor;
     private TextView mTextViewPorcentagem;
     private TextView mTextViewTotal;
@@ -24,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.estados, android.R.layout.simple_spinner_item);
         mSpinner.setAdapter(adapter);
-
         mEditTextValor = findViewById(R.id.editTextValor);
         mTextViewPorcentagem = findViewById(R.id.textViewPorcentagem);
         mTextViewTotal = findViewById(R.id.textViewTotal);
@@ -32,10 +32,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void calcular(View view) {
 
-        float porcentagem;
+        float porcentagem = 0;
 
         String estado = mSpinner.getItemAtPosition(mSpinner.getSelectedItemPosition()).toString();
         switch (estado) {
+            case "Selecione o Estado:":
+                Toast errorToast = Toast.makeText(this, "Erro! Selecione um Estado!", Toast.LENGTH_SHORT);
+                errorToast.show();
+                break;
             case "AC":
             case "MS":
             case "RO":
@@ -45,14 +49,12 @@ public class MainActivity extends AppCompatActivity {
                 porcentagem = 0.18f;
                 break;
         }
+        if(porcentagem != 0) {
+            mTextViewPorcentagem.setText(String.format("Porcentagem: %.0f%%", porcentagem * 100));
 
-        mTextViewPorcentagem.setText(String.format("%.0f%%", porcentagem * 100));
-
-        float valor = Float.parseFloat(mEditTextValor.getText().toString());
-        float total = valor + (valor * porcentagem);
-
-        mTextViewTotal.setText(String.format("Total com ICMS: R$ %.2f", total));
+            float valor = Float.parseFloat(mEditTextValor.getText().toString());
+            float total = valor + (valor * porcentagem);
+            mTextViewTotal.setText(String.format("Total com ICMS: R$ %.2f", total));
+        }
     }
-
-
 }
